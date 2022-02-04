a5f2d5cfd5d884eb06ac06413cd4b3405515e54b
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-02-04 04:26:35 +0000

    create test routes

---
+++ node_server/routes/test.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+router.use((req, res, next) => {
+    console.log('test dir');
+    next();
+});
+
+router.get('/ab?cd', (req, res) => {
+    res.send('/abcd or /acd');
+});
+
+router.get('/a+bc', (req, res) => {
+    res.send('/abc or /aabc or /aaabc ...etc');
+});
+
+router.get('/x*z', (req, res) => {
+    res.send('/xz or xyz or xabcdefdsdz ...etc');
+});
+
+router.get('/i_am(_not)?_you', (req, res) => {
+    res.send('i_am_you or i_am_not_you');
+});
+
+router.get(/test1/, (req, res) => {
+    res.send('include test1');
+});
+
+router.get(/.*test2$/, (req, res) => {
+    res.send('last test2');
+});
+
+/**
+ * URL : /users/100/item/200
+ *
+ * req.params : {"userId":"100","itemId":"200"}
+ */
+router.get('/users/:userId/item/:itemId', (req, res) => {
+    res.send(req.params);
+});
+
+/**
+ * URL : /map/tokyo-osaka
+ *
+ * req.params : {"from":"tokyo","to":"osaka"}
+ */
+router.get('/map/:from-:to', (req, res) => {
+    res.send(req.params);
+});
+
+exports.testRoutePath = '/test';
+exports.testRouter = router;
--- node_server/server/server.js
+++ node_server/server/server.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const path = require('path');
 
+// router
+const { testRoutePath, testRouter } = require('../routes/test');
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
@@ -11,11 +14,14 @@ const app = express();
 // publicのファイルをそれぞれ表示・読み込みできるように設定
 app.use(express.static('public'));
 
+// test配下のルーティング設定
+app.use(testRoutePath, testRouter);
+
 // 存在しないページの場合404ページにリダイレクトさせる
 app.use((req, res, next) => {
     res.status(404);
-    res.redirect('404.html');
-})
+    res.sendFile(path.join(__dirname, '../public/404.html'));
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
