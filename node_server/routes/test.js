1741a38a5fb186906fa69a5338500cb4095ccb66
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-02-07 04:37:34 +0000

    create get request routing

--- node_server/routes/test.js
+++ node_server/routes/test.js
@@ -30,6 +30,41 @@ router.get(/.*test2$/, (req, res) => {
     res.send('last test2');
 });
 
+const user_list = [
+    {
+        id: 100,
+        name: 'test1',
+        age: 20,
+    },
+    {
+        id: 200,
+        name: 'test2',
+        age: 25,
+    },
+    {
+        id: 300,
+        name: 'test3',
+        age: 30,
+    },
+]
+
+router.get('/get/:user_id', (req, res, next) => {
+    const userId = Number(req.params.user_id);
+    const findResult = user_list.find((user_obj) => {
+        return user_obj.id === userId;
+    });
+
+    if(!findResult){
+        res.status(404);
+        return res.end('not found! : ' + req.path);
+    }
+
+    const {age} = req.query;
+    age ? (findResult['ageDiff'] = Math.abs(findResult['age'] - age)) : null;
+
+    res.send(findResult);
+});
+
 /**
  * URL : /users/100/item/200
  *
