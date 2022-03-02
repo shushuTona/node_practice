22424d9d4c2c3d10db1b43b174bdb44c91c96ab5
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-03-02 06:05:51 +0000

    create 500 page

--- node_server/middleware/defaultError.js
+++ node_server/middleware/defaultError.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 // エラーが発生した際にステータスを500に設定してレスポンスを返す
 const errorMiddleware = (err, req, res, next) => {
     console.error(err);
-    res.status(500).send('Server Error!');
+    res.status(500);
+    res.sendFile(path.join(__dirname, '../public/500.html'));
 }
 
 module.exports = errorMiddleware;
