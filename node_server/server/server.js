79813fbc71bef792cbf1ca3acc29643ad6dcd783
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-03-18 06:04:05 +0000

    add session

--- node_server/server/server.js
+++ node_server/server/server.js
@@ -4,24 +4,40 @@ const express = require('express');
 const path = require('path');
 
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 
 // router
 const { testRoutePath, testRouter } = require('../routes/test');
 const { ejsTmpRoutePath, ejsTmpRouter } = require('../routes/ejsTmp');
 const { cookieTmpRoutePath, cookieTmpRouter } = require('../routes/cookie');
+const { sessionTmpRoutePath, sessionTmpRouter } = require('../routes/session');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
 
+// ejs使用設定
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// cookie設定
 app.use(cookieParser());
+
+// session設定
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+        maxAge: 1000 * 60 * 5 // 5分
+    }
+}));
 
 // publicのファイルをそれぞれ表示・読み込みできるように設定
 app.use(express.static('public'));
@@ -30,6 +46,7 @@ app.use(express.static('public'));
 app.use(testRoutePath, testRouter);
 app.use(ejsTmpRoutePath, ejsTmpRouter);
 app.use(cookieTmpRoutePath, cookieTmpRouter);
+app.use(sessionTmpRoutePath, sessionTmpRouter);
 
 app.use(require('../middleware/notFound'));
 
