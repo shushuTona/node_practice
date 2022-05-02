c32707ffa3723f87e126ae05020eced00bfc1901
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-05-02 17:41:21 +0900

    create transformStream file

---
+++ node_handson/chap3/transformStream.js
@@ -0,0 +1,53 @@
+const stream = require( 'stream' );
+
+class TransformStream extends stream.Transform {
+    remainting = '';
+
+    constructor( options ) {
+        super( {
+            readableObjectMode: true,
+            ...options
+        } );
+    }
+
+    _transform ( chunk, encoding, callback ) {
+        console.log( '===== _transform =====' );
+
+        const lines = ( chunk + this.remainting ).split( /\n/ );
+        this.remainting = lines.pop();
+        for (const line of lines) {
+            this.push( {
+                message: line,
+                delay: line.length * 100
+            } );
+        }
+
+        callback();
+    }
+
+    _flush ( callback ) {
+        console.log( '===== _flush =====' );
+
+        this.push( {
+            message: this.remainting,
+            delay: this.remainting.length * 100
+        } );
+
+        callback();
+    }
+}
+
+const transformStream = new TransformStream();
+transformStream
+    .on( 'readable', () => {
+        console.log( '===== readable =====' );
+
+        let chunk;
+        while ( ( chunk = transformStream.read() ) !== null ) {
+            console.log( chunk );
+        }
+    } );
+
+transformStream.write( 'Hello\n' );
+transformStream.write( 'Hello World' );
+transformStream.end();
