f8fd34b45190c5472b5c4b0831c0d08b9ec9524c
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-05-03 12:38:18 +0900

    create cluster multi process file

---
+++ node_handson/chap4/fibonacci.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function fibonacci ( n ) {
+    return n <= 1 ? n : fibonacci( n - 1 ) + fibonacci( n - 2 );
+}
---
+++ node_handson/chap4/multi-process.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { fork, setupMaster } = require( 'cluster' );
+
+console.log( 'main process', process.pid );
+
+// サブプロセスが実行するファイルを指定
+setupMaster( {
+    exec: `${ __dirname }/web-app`
+} );
+
+// CPUコアの数だけプロセスをフォークする
+const cpuCount = require( 'os' ).cpus().length;
+for ( let i = 0; i < cpuCount; i++ ) {
+    const sub = fork();
+    console.log( 'sub process', sub.process.pid );
+}
